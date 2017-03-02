b9ddb2f2d363108f0295cc9067756b6506ece0e3
laverntius <narugo901@gmail.com>
2017-03-02 21:55:28 +0000

    ynot

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.getElementById("orb").addEventListener("click", function(){ changeToNight()
-     changeToNight()
+     
 });
 function changeToNight(){
+     if(document.getElementById("orb").className == "sun"){
     document.getElementById("sky").className = "night";
-document.getElementById("orb").className = "moon";
-document.getElementById("moonspot1").className = "visible";
-document.getElementById("moonspot2").className = "visible";
-document.getElementById("moonspot3").className = "visible";
-
-    
+        document.getElementById("orb").className = "moon";
+        document.getElementById("moonspot1").className = "visible";
+        document.getElementById("moonspot2").className = "visible";
+        document.getElementById("moonspot3").className = "visible";
+} 
+else if(document.getElementById("orb").className == "moon"){
+        
+        document.getElementById("sky").className = "day";
+        document.getElementById("orb").className = "sun";
+        document.getElementById("moonspot1").className = "";
+        document.getElementById("moonspot2").className = "";
+        document.getElementById("moonspot3").className = "";        
+    }
 }
